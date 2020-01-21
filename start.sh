b21feadfd648a8d2e2c8da2cbeff95d3d084a5f7
Moringa School <loisemburu01@gmail.com>
2020-01-21 17:08:22 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export SECRET_KEY=4444
-export MAIL_USERNAME=loisemburu01@gmail.com
-export MAIL_PASSWORD=12345
+export SECRET_KEY='4444'
+export MAIL_USERNAME='loisemburu01@gmail.com'
+export MAIL_PASSWORD='12345'
 
 python3.7 manage.py server
